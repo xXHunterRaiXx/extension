117241a2cdf2adcae9e7ee9d1b38216fd2fbff70
xXHunterRaiXx <hunter.rai1221@gmail.com>
2025-04-24 15:33:39 -0500

    testing netlify issue 4

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
-import Button from "./button";
-import MyToggle from "./myToggle";
+import Button from "./Button.jsx";
+import MyToggle from "./myToggle.jsx";
 import { useState } from "react";
 
 function Card({
